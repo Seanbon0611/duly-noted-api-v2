393a19242ce4271074584a1755fc407c3324eb65
Seanbon0611 <seand0611@gmail.com>
2020-12-21 21:11:24 -0800

    connected database

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/jinzhu/gorm v1.9.16
+	gorm.io/driver/postgres v1.0.5
 	gorm.io/gorm v1.20.8
 )
--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
+	type User struct {
+		gorm.Model
+		Username string
+		Email    string
+		Password string
+	}
+
+	type Note struct {
+		gorm.Model
+		UserID  uint
+		Content string
+		User    User
+	}
+
+	db, err := gorm.Open(postgres.Open("host=localhost port=5431 user=postgres dbname=test_db sslmode=disable"), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+		panic("Failed to connect to database")
+	}
+	db.AutoMigrate(&User{}, &Note{})
+	fmt.Println("Database connected")
 	server := gin.Default()
+
+	server.GET("/:user/notes")
 
 	server.Run(":3001")
 }
