8e6dc8383aa8314d6cbdcbde7086e63a96e1f388
Seanbon0611 <seand0611@gmail.com>
2020-12-23 15:31:34 -0800

    added routes and getallusers func to retrieve all users

--- main.go
+++ main.go
@@ -2,29 +2,34 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+
+	models "duly_noted/models"
 )
 
-func main() {
+var db *gorm.DB
 
-	type User struct {
-		gorm.Model
-		Username string `gorm:"unique; not null"`
-		Email    string `gorm:"unique; not null"`
-		Password string `gorm:"not null`
+func getUsers(c *gin.Context) {
+	var users []models.User
+	db.Find(&users)
+
+	if len(users) <= 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "No users found"})
+		return
 	}
+	for _, user := range users {
 
-	type Note struct {
-		gorm.Model
-		UserID  uint
-		Content string
-		User    User
+		users = append(users, models.User{Username: user.Username, Email: user.Email})
 	}
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": users})
+}
 
+func init() {
 	db, err := gorm.Open(postgres.Open("host=localhost port=5431 user=postgres dbname=duly_noted sslmode=disable"), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
@@ -32,15 +37,22 @@ func main() {
 		fmt.Println(err)
 		panic("Failed to connect to database")
 	}
-	db.AutoMigrate(&User{}, &Note{})
+	db.AutoMigrate(&models.User{}, &models.Note{})
 
-	test_user := User{Username: "testuser3", Password: "password"}
-	db.Create(&test_user)
-	fmt.Println(test_user.ID)
 	fmt.Println("Database connected")
+}
+
+func main() {
+
 	server := gin.Default()
 
-	server.GET("/:username/notes")
-
-	server.Run(":3001")
+	v1 := server.Group("/api/v1")
+	{
+		v1.GET("/users", getUsers)
+		// 	v1.GET("/users/id", getSingleUser)
+		// 	v1.POST("/users/create", createUser)
+		// 	v1.DELETE("users/delete/:id", deleteUser)
+		// }
+		server.Run(":3001")
+	}
 }
