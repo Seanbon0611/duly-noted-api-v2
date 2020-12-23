991b87232bc9e6f9c2bb0e9e05b9463a4f1517b7
Seanbon0611 <seand0611@gmail.com>
2020-12-23 15:30:55 -0800

    added models to hold model struct for db

---
+++ models/models.go
@@ -0,0 +1,19 @@
+package models
+
+import "gorm.io/gorm"
+
+type (
+	User struct {
+		gorm.Model
+		Username string `json:"username" binding:"min=3, max=25" gorm:"varchar(27)unique; not null"`
+		Email    string `json:"email"gorm:"unique; not null"`
+		Password string `gorm:"not null`
+	}
+
+	Note struct {
+		gorm.Model
+		UserID  uint   `json:"user_id"`
+		Content string `json:"content`
+		User    User
+	}
+)
