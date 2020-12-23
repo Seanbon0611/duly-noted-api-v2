dee9fd3bad3ab196f9c9268e9f447e34e985e661
Seanbon0611 <seand0611@gmail.com>
2020-12-22 21:27:45 -0800

    added logger that will display sql queries

--- main.go
+++ main.go
@@ -6,14 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func main() {
+
 	type User struct {
 		gorm.Model
-		Username string
-		Email    string
-		Password string
+		Username string `gorm:"unique; not null"`
+		Email    string `gorm:"unique; not null"`
+		Password string `gorm:"not null`
 	}
 
 	type Note struct {
@@ -23,16 +25,22 @@ func main() {
 		User    User
 	}
 
-	db, err := gorm.Open(postgres.Open("host=localhost port=5431 user=postgres dbname=test_db sslmode=disable"), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open("host=localhost port=5431 user=postgres dbname=duly_noted sslmode=disable"), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		fmt.Println(err)
 		panic("Failed to connect to database")
 	}
 	db.AutoMigrate(&User{}, &Note{})
+
+	test_user := User{Username: "testuser3", Password: "password"}
+	db.Create(&test_user)
+	fmt.Println(test_user.ID)
 	fmt.Println("Database connected")
 	server := gin.Default()
 
-	server.GET("/:user/notes")
+	server.GET("/:username/notes")
 
 	server.Run(":3001")
 }
