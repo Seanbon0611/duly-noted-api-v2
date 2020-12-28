c5637ea8c9076f859dedaa5099d46211922c8752
Seanbon0611 <seand0611@gmail.com>
2020-12-27 18:34:18 -0800

    added createUSer function to add new user to DB

--- Controllers/User.go
+++ Controllers/User.go
@@ -1,13 +1,18 @@
 package Controllers
 
 import (
-	"net/http"
-
 	Config "duly_noted/Config"
 	Models "duly_noted/Models"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+type CreateUserInput struct {
+	Username string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
 
 func getUsers(c *gin.Context) {
 	var users []Models.User
@@ -22,4 +27,15 @@ func getUsers(c *gin.Context) {
 		users = append(users, Models.User{Username: user.Username, Email: user.Email})
 	}
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": users})
+}
+
+func CreaetUser(c *gin.Context) {
+	var input CreateUserInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	new_user := Models.User{Username: input.Username, Email: input.Email, Password: input.Password}
+	Config.DB.Create(&new_user)
+	c.JSON(http.StatusOK, gin.H{"data": new_user})
 }
