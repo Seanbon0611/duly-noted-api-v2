371446fdde2bbbd3d65237849e90a94ddca8297b
Seanbon0611 <seand0611@gmail.com>
2020-12-28 15:03:36 -0800

    added functions to models that will hash passwords using bcrypt

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module duly_noted
 go 1.15
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.6.3
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 	gorm.io/driver/postgres v1.0.5
 	gorm.io/gorm v1.20.8
 )
--- models/models.go
+++ models/models.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -19,3 +20,19 @@ type (
 		User    User
 	}
 )
+
+func (user *User) HashPassword(password string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return err
+	}
+	user.Password = string(bytes)
+	return nil
+}
+
+func (user *User) CheckPassword(password string) error {
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		return err
+	}
+	return nil
+}
