090a345aa256447ed8b986acf7e65dc0d294aff7
Seanbon0611 <seand0611@gmail.com>
2020-12-28 14:18:57 -0800

    removed unnecessary code and added comments to better understand what each codeblock does

--- main.go
+++ main.go
@@ -8,17 +8,19 @@ import (
 )
 
 func main() {
+	//Initiate server
 	server := gin.Default()
+
+	//Connection to Database
 	Config.Init()
-	server.Use(func(c *gin.Context) {
-		c.Set("db", Config.DB)
-		c.Next()
-	})
+
+	//Routes
 	server.GET("/api/v1/users", Controllers.GetUsers)
 	server.GET("/api/v1/users/:id", Controllers.GetSingleUser)
 	server.POST("/api/v1/users/create", Controllers.CreateUser)
 	server.DELETE("/api/v1/users/delete/:id", Controllers.DeleteUser)
-	// }
+
+	//Start Server
 	server.Run(":3001")
-	// Routes.Router()
+
 }
