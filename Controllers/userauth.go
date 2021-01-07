110c64a384e09f2cbc1f6c367d1962616a7dd5ad
Seanbon0611 <seand0611@gmail.com>
2021-01-06 22:44:16 -0800

    added controller file that handles user login

---
+++ Controllers/userauth.go
@@ -0,0 +1,77 @@
+package controllers
+
+import (
+	auth "duly_noted/Auth"
+	"duly_noted/config"
+	"duly_noted/models"
+	"net/http"
+
+	"gorm.io/gorm"
+
+	"github.com/gin-gonic/gin"
+)
+
+type (
+	LoginPayload struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	LoginResponse struct {
+		Token string `json:"token"`
+	}
+)
+
+func Login(c *gin.Context) {
+	var user models.User
+	var payload LoginPayload
+
+	//Checks Token
+	err := c.ShouldBindJSON(&payload)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		c.Abort()
+		return
+	}
+
+	//Query to find user where email == user input email param
+	result := config.DB.Where("email = ?", payload.Email).First(&user)
+
+	//if credentials are invalid, return error
+	if result.Error == gorm.ErrRecordNotFound {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Incorrect credentials"})
+		c.Abort()
+		return
+	}
+
+	//Checking user input password
+	err = user.CheckPassword(payload.Password)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		c.Abort()
+		return
+	}
+
+	jwtWrapper := auth.JwtWrapper{
+		SecretKey:       "thesecretestofkeys",
+		Issuer:          "AuthService",
+		ExpirationHours: 2,
+	}
+
+	//generate uer token
+	token, err := jwtWrapper.GenerateToken(user.Email)
+	//If there is an error return 500 status code
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error signing token"})
+		c.Abort()
+		return
+	}
+
+	tokenResponse := LoginResponse{
+		Token: token,
+	}
+
+	c.JSON(http.StatusOK, tokenResponse)
+
+	return
+}
