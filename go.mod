03051d2dce982539e7f131a67cea18c25b4647b0
Seanbon0611 <seand0611@gmail.com>
2021-01-11 15:19:06 -0800

    reconfigured CORS settings, tested with front end and account successfully created

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 	gorm.io/driver/postgres v1.0.5
--- main.go
+++ main.go
@@ -4,13 +4,25 @@ import (
 	"duly_noted/config"
 	"duly_noted/controllers"
 	"duly_noted/models"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	//Initiate server
 	server := gin.Default()
+
+	//Cors
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	//Connection to Database
 	config.Init()
