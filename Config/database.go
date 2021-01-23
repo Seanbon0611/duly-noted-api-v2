4a23ca46c6241241d37acad882dbb58f335230ba
Seanbon0611 <seand0611@gmail.com>
2021-01-22 19:18:10 -0800

    added docker file

--- Config/database.go
+++ Config/database.go
@@ -12,7 +12,7 @@ var DB *gorm.DB
 
 //Initiate Database
 func Init() {
-	database, err := gorm.Open(postgres.Open("host=dulynoted.c3jtcwuysqgr.us-west-1.rds.amazonaws.com port=5432 user=postgres dbname=dulynoted password=dulynoteddbpassword sslmode=disable"), &gorm.Config{
+	database, err := gorm.Open(postgres.Open("host=dulynoted.c3jtcwuysqgr.us-west-1.rds.amazonaws.com port=5432 user=postgres dbname=postgres password=dulynoteddbpassword sslmode=disable"), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info), //Logs all SQL queries into console
 	})
 	if err != nil {
