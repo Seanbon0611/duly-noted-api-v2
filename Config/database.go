a226bd17b7bca07b6e520f001799c210c84ff0b1
Seanbon0611 <seand0611@gmail.com>
2021-03-05 19:40:48 -0800

    deploying

--- Config/database.go
+++ Config/database.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	// "github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -13,7 +14,12 @@ var DB *gorm.DB
 
 //Initiate Database
 func Init() {
-	//envirnment variables for credentials
+	os.Setenv("HOST", "dulynoted.c3jtcwuysqgr.us-west-1.rds.amazonaws.com")
+	os.Setenv("PORT", "5432")
+	os.Setenv("DBNAME", "postgres")
+	os.Setenv("DBUSER", "postgres")
+	os.Setenv("PASSWORD", "J3tBlu30611")
+	os.Setenv("SSLMODE", "disable")
 
 	dbHost := os.Getenv("HOST")
 	dbPort := os.Getenv("PORT")
