f39c324436250098e52d6729dc9bb32c173d2f6e
Seanbon0611 <seand0611@gmail.com>
2021-03-08 14:16:37 -0800

    fixed bug regarding CORS

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	//Cors
 	server.Use(cors.New(cors.Config{
-		AllowOrigins:  []string{"http://localhost:3000, https://angry-bohr-320bd2.netlify.app/, https://duly-noted-api-cfac6.ondigitalocean.app/duly-noted-api-v-2"},
+		AllowOrigins:  []string{"https://angry-bohr-320bd2.netlify.app", "http://localhost:3000"},
 		AllowMethods:  []string{"PUT", "PATCH", "POST", "GET"},
 		AllowHeaders:  []string{"Origin"},
 		ExposeHeaders: []string{"Content-Length"},
