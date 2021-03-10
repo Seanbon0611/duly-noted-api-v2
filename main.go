c43ef8c006a188e81464e62557372ea58c779af9
Seanbon0611 <seand0611@gmail.com>
2021-03-10 13:05:27 -0800

    adding OPTIONS as an allowed HTTP method

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	//Cors
 	server.Use(cors.New(cors.Config{
 		AllowOrigins:  []string{"https://angry-bohr-320bd2.netlify.app", "http://localhost:3000"},
-		AllowMethods:  []string{"PUT", "PATCH", "POST", "GET", "DELETE"},
+		AllowMethods:  []string{"PUT", "PATCH", "POST", "GET", "DELETE", "OPTIONS"},
 		AllowHeaders:  []string{"Origin"},
 		ExposeHeaders: []string{"Content-Length"},
 		MaxAge:        3 * time.Hour,
